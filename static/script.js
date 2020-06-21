6d782f4aef55cc278cd6d9fd06eec652f6b1a2f2
Sebastian <Ferreira>
2020-06-20 23:32:40 -0300

    add functions to order by name, total cases and active

--- static/script.js
+++ static/script.js
@@ -1,12 +1,17 @@
 const API_URL = 'https://corona.lmao.ninja/v2/countries?sort=country';
+let countries=[];
 
 $.getJSON(API_URL, (data)=>{
     //console.log(data);
-    showInfo(data);
+    countries = data;
+    showInfo();
 })
 
-const showInfo = (countries) =>{
+
+const showInfo = () =>{
     //console.log(countries);
+    $('#covidTable tbody').empty();
+    countries.reverse();
     countries.forEach((country, index)=>{
         $('#covidTable tbody').append(`<tr>
                                             <th scope="row">${index+1}</th>
@@ -17,7 +22,34 @@ const showInfo = (countries) =>{
                                             <td>${country.cases}</td>
                                             <td>${country.active}</td>
                                         </tr>`);
-        
     })
 }
 
+const orderMaxTotalCases=()=>{
+    countries.sort(function(a, b){
+        let a1 = a.cases;
+        let b1 = b.cases;
+        if(a1==b1) return 0;
+        return a1>b1?1:-1;
+    })
+    showInfo();
+}
+const orderByName=()=>{
+    countries.sort(function(a, b){
+        let a1 = a.country;
+        let b1 = b.country;
+        if(a1==b1) return 0;
+        return a1>b1?1:-1;
+    })
+    countries.reverse();
+    showInfo();
+}
+const orderByActiveCases=()=>{
+    countries.sort(function(a, b){
+        let a1 = a.active;
+        let b1 = b.active;
+        if(a1==b1) return 0;
+        return a1>b1?1:-1;
+    })
+    showInfo();
+}
